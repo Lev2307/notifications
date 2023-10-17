2ca552cf54457a658c731cd172d55b6f840a9b6d
Lev2307 <Abrosimow1@mail.ru>
2023-10-17 16:20:33 +0300

    add nginx and gunicorn

--- notifications/config/settings.py
+++ notifications/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True # False for prod
+DEBUG = int(os.environ.get('DEBUG')) # False for prod
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
